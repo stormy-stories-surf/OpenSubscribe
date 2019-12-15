3421b0b93aa03a25b4cad5e39ce4903745ed6f5a
mezorian <mezorian@inc-ffh.solutions>
2019-12-15 22:13:53 +0100

    add parameters to Open-Subscribe

--- python/OpenSubscribe.py
+++ python/OpenSubscribe.py
@@ -1,13 +1,38 @@
-import smtplib, ssl
+import argparse
+import smtplib
+import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+
 class OpenSubscribe:
-    def __init__(self, smtpServer_ ,port_ ,smtpUser_ ,smtpPassword_ ):
+    def __init__(self, smtpServer_, port_, smtpUser_, smtpPassword_):
         self.smtp_server = smtpServer_
         self.port = port_  # For starttls
         self.sender_email = smtpUser_
         self.password = smtpPassword_
+
+    def setup(self, configFileName_ = "mail-templates/confirmSubscribtion.html"):
+        print("Im in setup function")
+        self.replaceStringInFile(configFileName_,
+                            "<PUT_YOUR_URL_HERE>", "https://stormy-stories.surf")
+
+    def replaceStringInFile(self, filename, old_string, new_string):
+        # Safely read the input filename using 'with'
+        with open(filename, 'r+') as f:
+            s = f.read()
+            if old_string not in s:
+                print('"{old_string}" not found in {filename}.'.format(**locals()))
+                return
+
+        # Safely write the changed content, if found in the file
+        #with open(filename, 'w') as f:
+        #    s2 = f.read()
+        #    print(
+        #        'Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals()))
+            s = s.replace(old_string, new_string)
+            print(s)
+            f.write(s)
 
     def smtpLogin(self):
         # Create a secure SSL context
@@ -15,21 +40,22 @@ class OpenSubscribe:
 
         # Try to log in to server and send email
         try:
-            self.server = smtplib.SMTP(self.smtp_server,self.port)
-            self.server.ehlo() # Can be omitted
-            self.server.starttls(context=self.context) # Secure the connection
-            self.server.ehlo() # Can be omitted
+            self.server = smtplib.SMTP(self.smtp_server, self.port)
+            self.server.ehlo()  # Can be omitted
+            self.server.starttls(context=self.context)  # Secure the connection
+            self.server.ehlo()  # Can be omitted
             self.server.login(self.sender_email, self.password)
 
         except Exception as e:
             # Print any error messages to stdout
             print(e)
 
-    def sendConfirmSubscribtionMail(self,receipient_, subscribtionID_):
+    def sendConfirmSubscribtionMail(self, receipient_, subscribtionID_):
         try:
             receipientsList = [receipient_]
             receipients = ", ".join(receipientsList)
-            message = MIMEMultipart("Please confirm your subscribtion for STORMY-STORIES.SURF")
+            message = MIMEMultipart(
+                "Please confirm your subscribtion for STORMY-STORIES.SURF")
             message["Subject"] = "Please confirm your subscribtion for STORMY-STORIES.SURF"
             message["From"] = self.sender_email
             message["To"] = receipients
@@ -37,11 +63,11 @@ class OpenSubscribe:
             # Create the plain-text and HTML version of your message
             with open('confirmSubscribtion.txt', 'r') as file:
                 text = file.read()
-                text = text.replace("XXXXX",subscribtionID_)
+                text = text.replace("XXXXX", subscribtionID_)
 
             with open('confirmSubscribtion.html', 'r') as file:
                 html = file.read()
-                html = html.replace("XXXXX",subscribtionID_)
+                html = html.replace("XXXXX", subscribtionID_)
 
             # Turn these into plain/html MIMEText objects
             part1 = MIMEText(text, "plain")
@@ -52,7 +78,8 @@ class OpenSubscribe:
             message.attach(part1)
             message.attach(part2)
 
-            self.server.sendmail(self.sender_email, receipients, message.as_string())
+            self.server.sendmail(
+                self.sender_email, receipients, message.as_string())
             print("Successfully sent email")
 
         except Exception as e:
@@ -62,7 +89,7 @@ class OpenSubscribe:
     def smtpClose(self):
         self.server.quit()
 
-	def getMailAddressesWithoutConfirmation(self):
+    def getMailAddressesWithoutConfirmation(self):
         print("TO BE CONTINUED")
 
         import mysql.connector
@@ -76,12 +103,30 @@ class OpenSubscribe:
 
         mycursor = mydb.cursor()
 
-        mycursor.execute("SELECT mailaddress, subscribeID FROM subscriber WHERE confirmationMailSent = 0 ")
+        mycursor.execute(
+            "SELECT mailaddress, subscribeID FROM subscriber WHERE confirmationMailSent = 0 ")
 
         myresult = mycursor.fetchall()
 
         for x in myresult:
-            mailaddress=x[0]
-            subscribeID=x[1]
+            mailaddress = x[0]
+            subscribeID = x[1]
             print("Mail : " + mailaddress)
             print("SubscribeID : " + subscribeID)
+
+    def parseArgs(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '--setup', action='store_true',
+             help='Setup OpenSubscribe with options set in config.json')
+        args = parser.parse_args()
+        return args
+
+def main():
+    s = OpenSubscribe("<PUT_YOUR_SMTP_SERVER_HERE>", "<PUT_YOUR_SMTP_PORT_HERE>" , "<PUT_YOUR_SENDER_MAIL_ADDRESS_HERE>", "<PUT_YOUR_SENDER_PASSWORD_HERE>")
+    args = s.parseArgs()
+    if args.setup:
+        s.setup()
+
+if __name__ == '__main__':
+    main()
