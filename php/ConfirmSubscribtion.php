5f0e344f3676e6b1f55418a1a974bb587dddfeeb
mezorian <mezorian@inc-ffh.solutions>
2019-12-24 01:03:57 +0100

    remove echos from php/ConfirmSubscribtion.php

--- php/ConfirmSubscribtion.php
+++ php/ConfirmSubscribtion.php
@@ -1,26 +1,6 @@
 Yehaaa.. you succesfully subscribed to our e-mail newsletter!
 <?php
 
-function interpolateQuery($query, $params) {
-    echo "params " . $params . "<br/ >";
-    $keys = array();
-
-    # build a regular expression for each parameter
-    foreach ($params as $key => $value) {
-        if (is_string($key)) {
-            $keys[] = '/:'.$key.'/';
-        } else {
-            $keys[] = '/[?]/';
-        }
-    }
-
-    $query = preg_replace($keys, $params, $query, 1, $count);
-
-    #trigger_error('replaced '.$count.' keys');
-
-    return $query;
-}
-
 function update_subscribtion_confirmed($subscribeID_p) {
         // open database connection
 	$pdo_l = new PDO('mysql:host=localhost;dbname=OpenSubscribe', 'ConfirmSubscribtionUser', '<PUT_YOUR_CONFIRM_SUBSCRIBTION_USER_PASSWORD_HERE>');
@@ -29,13 +9,13 @@ function update_subscribtion_confirmed($subscribeID_p) {
   $sql_l = "UPDATE subscriber SET subscribtionConfirmed=true WHERE subscribeID=?";
   $pdo_l->prepare($sql_l)->execute([$subscribeID_p]);
 
-	echo "Updated newsletter subscribtion with subscribeID " . $subscribeID_p . "<br />";
+	//echo "Updated newsletter subscribtion with subscribeID " . $subscribeID_p . "<br />";
 }
 
 // main
 if ($_GET["data"] != "") {
 	$subscribtionID = $_GET["data"];
-  echo "ID is " . $subscribtionID . "<br />";
+  //echo "ID is " . $subscribtionID . "<br />";
 	update_subscribtion_confirmed($subscribtionID);
 }
 
