17f7fcfea09bcd297132f9ab17db751e44da1099
mezorian <mezorian@inc-ffh.solutions>
2019-12-28 15:42:24 +0100

    update setupDatabase.sql

--- sql/setupDatabase.sql
+++ sql/setupDatabase.sql
@@ -18,6 +18,17 @@ CREATE TABLE subscriber (
    unSubscribedMailSent boolean
 );
 
+CREATE TABLE newsletterMail (
+   id INT AUTO_INCREMENT primary key NOT NULL,
+   url text NOT NULL,
+   pathTXT text NOT NULL,
+   pathHTML text NOT NULL,
+   confirmationMailSent boolean,
+   subscribtionConfirmed boolean,
+   unSubscribed boolean,
+   unSubscribedMailSent boolean
+);
+
 /* table which stores the timestamp of the
    last subscribtion to avoid DDOS */
 DROP TABLE IF EXISTS lastSubscribtion;
@@ -33,10 +44,10 @@ DROP USER IF EXISTS 'SubscribtionFormUser'@'localhost';
 DROP USER IF EXISTS 'SendMailsUser'@'localhost';
 DROP USER IF EXISTS 'UnsubscribeUser'@'localhost';
 
-CREATE USER 'ConfirmSubscribtionUser'@'localhost' IDENTIFIED BY 'TfLtK36uNteCBGhR8Bx9CNfP6tgNw6DXpy3vfVDJSXmyGvuxBLQNivCXP8cb4S8f';
-CREATE USER 'SubscribtionFormUser'@'localhost' IDENTIFIED BY 'qKRDZBx3pKajwahSkZxarSYViTPSAprY3nHwn5P7MSPHnDQE35YGXKpVWUq4KR9n';
-CREATE USER 'SendMailsUser'@'localhost' IDENTIFIED BY '2rdhVmePV3uZ7GqEWEWXRWekCXSrwSrHbSkvqksWbsDMeqr8jwAxsDT9VSKEqDQh';
-CREATE USER 'UnsubscribeUser'@'localhost' IDENTIFIED BY 'LJ5jF8jGhkedA4Vn7xqADu37zFSZSJKc6jPvLwrHUY6geuNyaLogw2s324nUWvMC';
+CREATE USER 'ConfirmSubscribtionUser'@'localhost' IDENTIFIED BY '<PUT_YOUR_CONFIRM_SUBSCRIBTION_USER_PASSWORD_HERE>';
+CREATE USER 'SubscribtionFormUser'@'localhost' IDENTIFIED BY '<PUT_YOUR_SUBSCRIBTION_FORM_USER_PASSWORD_HERE>';
+CREATE USER 'SendMailsUser'@'localhost' IDENTIFIED BY '<PUT_YOUR_SEND_MAILS_USER_PASSWORD_HERE>';
+CREATE USER 'UnsubscribeUser'@'localhost' IDENTIFIED BY '<PUT_YOUR_UNSUBSCRIBE_USER_PASSWORD_HERE>';
 
 /* GRANT ACCESS TO TABLES */
 GRANT UPDATE ON OpenSubscribe.subscriber TO 'ConfirmSubscribtionUser'@'localhost';
